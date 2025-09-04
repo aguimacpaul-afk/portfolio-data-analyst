3992706f8b1e69b5efdee7266dc390b4ccb4c4e8
Paul Aguilar <aguimac.paul@gmail.com>
2025-09-04 11:57:11 -0500

    Update queries.sql

--- Murder Mystery/queries.sql
+++ Murder Mystery/queries.sql
@@ -7,8 +7,15 @@ from get_fit_now_member m
 join get_fit_now_check_in n on m.id = n.membership_id
 group by m.id, m.name
 having count(n.check_in_date) >10
-order by total_checkins desc;
-
+order by total_checkins desc Limit 5;
+/*
+member_id|name          |total_checkins|
+---------+--------------+--------------+
+XTE42    |Sarita Bartosh|            30|
+X1IYH    |Tu Lipkovitch |            30|
+C84S4    |Mervin Eacret |            30|
+C581L    |Monty Torrico |            30|
+S702J    |Ladawn Boards |            30|*/
 
 /*Personas con ingresos altos
 De la tabla income, selecciona las personas con ingresos por encima del percentil 90. 
@@ -24,6 +31,14 @@ select p.name, p.address_number, p.address_street_name, i.annual_income from per
 join income i on p.ssn = i.ssn 
 CROSS JOIN p90
 WHERE i.annual_income > p90.cutoff;
+/*
+name               |address_number|address_street_name|annual_income|
+-------------------+--------------+-------------------+-------------+
+Sandy Braim        |          1844|Gigi Blvd          |        88500|
+Candra Laguardia   |          2839|Trevelyan Ave      |        88800|
+Dominique Kelcourse|          1592|South Baybrook Blvd|        89300|
+Alysha Laba        |          1036|Pala Ave           |       324300|
+Rogelio Trullinger |          1462|Sherando Way       |        88300|*/
 
 /*Eventos más concurridos
 Usa la tabla facebook_event_checkin para encontrar los 3 eventos con mayor número de asistentes distintos.
@@ -33,7 +48,14 @@ select event_name, count(distinct person_id) as num_asistentes
 from facebook_event_checkin fec 
 group by event_name 
 order by num_asistentes desc
-
+/*
+event_name                     |num_asistentes|
+-------------------------------+--------------+
+¶                              |           278|
+                -- Mark Twain¶ |           139|
+                -- Oscar Wilde¶|           103|
+SQL Symphony Concert           |            89|
+We wish you a Hare Krishna¶    |            67|*/
 /*
 Personas con múltiples licencias
 Detecta personas en drivers_license que tengan más de una licencia registrada.
@@ -41,7 +63,10 @@ Detecta personas en drivers_license que tengan más de una licencia registrada.
 select id, count(distinct p.license_id ) from person p  
 group by p.id  
 having count(distinct p.license_id)  > 1
-
+/*
+	id|count|
+--+-----+
+*/
 
 /*Cruce sospechoso
 Combina crime_scene_report con person para listar a las personas que vivan en la misma calle
@@ -51,7 +76,14 @@ donde ocurrió un crimen.
 select p.name , p.address_number, p.address_street_name, csr.date, csr.type, csr.city    
 from person p join crime_scene_report csr 
 on p.address_street_name like '%'||csr.city||'%'
-
+/*
+name            |address_number|address_street_name    |date    |type     |city      |
+----------------+--------------+-----------------------+--------+---------+----------+
+Cory Rodriquz   |          2024|Montgomery Rd          |20170202|smuggling|Montgomery|
+Cory Rodriquz   |          2024|Montgomery Rd          |20170203|smuggling|Montgomery|
+Cory Rodriquz   |          2024|Montgomery Rd          |20170205|robbery  |Montgomery|
+Cory Rodriquz   |          2024|Montgomery Rd          |20170403|fraud    |Montgomery|
+Charity Bleicher|          2550|Thomas Jefferson Circle|20170207|arson    |Jefferson |*/
 
 /*
 Ranking de ingresos por ciudad
@@ -76,10 +108,15 @@ JOIN
     ON p.address_street_name LIKE '%' || c.city || '%'
 ORDER BY 
     ranking;
- 
 /*
-
-Entrevistas relacionadas con un crimen
+city  |name           |annual_income|ranking|
+------+---------------+-------------+-------+
+Akron |Dean Threlkeld |        53800|      1|
+Akron |Dean Threlkeld |        53800|      1|
+Akron |Dean Threlkeld |        53800|      1|
+Albany|Dwight Mcvinney|        12800|      1|
+Akron |Dean Threlkeld |        53800|      1|*/
+/*Entrevistas relacionadas con un crimen
 Une interview con crime_scene_report para encontrar entrevistas de personas relacionadas con 
 un crimen ocurrido en la misma fecha.*/
 
@@ -92,6 +129,9 @@ select i.transcript, csr.date, csr.type, csr.description, p.name  from interview
 join person p on i.person_id = p.id
 JOIN crime_scene_report csr
     ON p.address_street_name LIKE '%' || csr.description || '%'; 
+/*
+transcript|date|type|description|name|
+----------+----+----+-----------+----+*/
 
 
 
